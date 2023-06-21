0943c384189163cb61ed0d0f2a6f0086b40f44d1
shashankmarch27 <shashankmarch27@gmail.com>
2023-06-22 00:20:45 +0530

    fixed sbus read code

--- src/main.cpp
+++ src/main.cpp
@@ -13,77 +13,58 @@ unsigned long end_pulse = 0;
 unsigned long pulse_width = 0;
 
 // put object declarations here:
-bfs::SbusRx sbus_rx(&Serial2,16,17,false);
+sbus receive(&Serial2,17,16); 
 
 // put function declarations here:
-// void IRAM_ATTR read_ppm_signal();
+void IRAM_ATTR read_ppm_signal();
 
 void setup(){
   // put your setup code here, to run once:
   Serial.begin(115200);
   pinMode(PPM_PIN, INPUT_PULLUP);
-  sbus_rx.Begin();
-  // attachInterrupt(PPM_PIN, read_ppm_signal, FALLING);
+  receive.init();
+  attachInterrupt(PPM_PIN, read_ppm_signal, FALLING);
 }
 
 void loop(){
   // put your main code here, to run repeatedly:
-  if (sbus_rx.Read()) {
-    /* Grab the received data */
-    data = sbus_rx.data();
-    /* Display the received data */
-    for (int8_t i = 0; i < data.NUM_CH; i++) {
-      Serial.print(data.ch[i]);
-      Serial.print("\t");
-    }
-    /* Display lost frames and failsafe data */
-    Serial.print(data.lost_frame);
-    Serial.print("\t");
-    Serial.println(data.failsafe);
-    /* Set the SBUS TX data to the received data */
-    sbus_tx.data(data);
-    /* Write the data to the servos */
-    sbus_tx.Write();
-  }
+  receive.read();
 
-  Serial.print(channels_received[0]);
+  Serial.print(receive.data[AILERON]);
   Serial.print(" ");
-  Serial.print(channels_received[1]);
+  Serial.print(receive.data[ELEVATOR]);
   Serial.print(" ");
-  Serial.print(channels_received[2]);
+  Serial.print(receive.data[THROTTLE]);
   Serial.print(" ");
-  Serial.print(channels_received[3]);
-  Serial.print(" ");
-  Serial.print(channels_received[4]);
+  Serial.print(receive.data[RUDDER]);
   Serial.println(" ");
-  delay(100);
 }
 
-// void IRAM_ATTR read_ppm_signal(){
+void IRAM_ATTR read_ppm_signal(){
 
-//   start_pulse = end_pulse;
-//   end_pulse = micros();
-//   pulse_width = end_pulse - start_pulse;
-
-
-//   if (number_of_channels_read == -1)
-//   {// first rising edge seen
-//     number_of_channels_read = 0;
-//     return;
-//   }
-//   else if (pulse_width > T_SYNC)
-//   {
-//     number_of_channels_read = 0;
-//     return;
-//   }else   if (number_of_channels_read > NUMBER_OF_CHANNELS)
-//   {
-//     // sync bit starts
-//     number_of_channels_read = -1;
-//     return;
-//   }
-
-//   number_of_channels_read++;
-//   channels_received[number_of_channels_read - 1] = uint16_t(pulse_width);
+  start_pulse = end_pulse;
+  end_pulse = micros();
+  pulse_width = end_pulse - start_pulse;
 
 
-// }
+  if (number_of_channels_read == -1)
+  {// first rising edge seen
+    number_of_channels_read = 0;
+    return;
+  }
+  else if (pulse_width > T_SYNC)
+  {
+    number_of_channels_read = 0;
+    return;
+  }else   if (number_of_channels_read > NUMBER_OF_CHANNELS)
+  {
+    // sync bit starts
+    number_of_channels_read = -1;
+    return;
+  }
+
+  number_of_channels_read++;
+  channels_received[number_of_channels_read - 1] = uint16_t(pulse_width);
+
+
+}
