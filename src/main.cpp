4a903bb28bdfb00cbcea6f4f88c46e27fe8d5bd6
shashankmarch27 <shashankmarch27@gmail.com>
2023-06-23 15:35:23 +0530

    some visual changes

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,27 @@
 #include <ppm.h>
 #include "OTA.h"
 
+// replace by your credentials
 #define SSID "RLRS"
 #define PASSWORD "robolrs"
 
 // put object declarations here:
+
 // sbus receive(&Serial2,17,16); 
-ppm receive(35);
+ppm receive(16);
+
 // put function declarations here:
-
-
 
 void setup(){
   // put your setup code here, to run once:
-  setupOTA("RoboLRS", SSID, PASSWORD);
+  Serial.begin(115200);
+  setupOTA("RoboLRS TX", SSID, PASSWORD);
   receive.init();
 }
 
 void loop(){
   // put your main code here, to run repeatedly:
+
   ArduinoOTA.handle();  // call this line at regular interval for ota functionality
   receive.read();
 
